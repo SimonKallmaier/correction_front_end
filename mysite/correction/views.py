cbe5dc5a105df5eb6a3812618f59359d1a8037d2
Simon Kallmaier <simon.kallmaier@web.de>
2022-07-21 20:48:06 +0200

    django tutorial to chapt 4

--- mysite/correction/views.py
+++ mysite/correction/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.views import generic
 
-# Create your views here.
+from .models import OcrExtractedText
+
+
+class IndexView(generic.DetailView):
+    template_name = "correction/index.html"
+    model = OcrExtractedText
+
+
+class TextView(generic.ListView):
+    template_name = "correction/show_text.html"
+    context_object_name = "ocr_text"
+    
+    def get_queryset(self):
+        return OcrExtractedText.objects.order_by('-pub_date')[-1]
